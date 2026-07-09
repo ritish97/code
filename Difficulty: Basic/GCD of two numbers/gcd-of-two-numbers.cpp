2482937d519cdd593125f65c60062aed0a140e27
ritish97 <yadavretishkumar05@gmail.com>
2026-07-09 22:38:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -2,11 +2,12 @@
 class Solution {
   public:
     int gcd(int a, int b) {
-        while (b!=0){
-            int remainder = a%b;
-            a = b;
-            b = remainder;
+        while (a>0&&b>0){
+            if (a>b) a = a%b;
+            else b = b % a;
+            
         }
+        if(a==0) return b;
         return a;
     }
 };
