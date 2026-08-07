4cd0b80d31ad9dd93660202fefa8d791f099b435
ritish97 <yadavretishkumar05@gmail.com>
2026-08-07 21:36:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking Input/taking-input.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline ( cin , s);
+    int n;
+    cin >>n;
+    float f; 
+    cin >> f;
+    
+
+    cout << s << endl;
+    cout << n << endl;
+    cout <<  int (f) << endl;
+    return 0;
+}
