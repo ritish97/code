b0be92f9baab4305418e8eef377a5b5c876c8e27
ritish97 <yadavretishkumar05@gmail.com>
2026-09-08 23:41:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nth Root of M/nth-root-of-m.cpp
+++ Difficulty: Medium/Nth Root of M/nth-root-of-m.cpp
@@ -1,29 +1,29 @@
 class Solution {
   public:
+  int func(int mid,int n,int m){
+      long long ans =1;
+      for(int i = 1;i<=n;i++){
+          ans = ans*mid;
+          if(ans>m) return 2;
+      }
+      if (ans==m) return 1;
+      return 0;
+  }
     int nthRoot(int n, int m) {
-        if (m==0) return 0;
-    
-
-                for (int i = 1; i <= m; i++) {
-
-                    long long value = 1;
-
-                    for (int j = 1; j <= n; j++) {
-                        value *= i;
-
-                        if (value > m)
-                            break;
-                    }
-
-                    if (value == m)
-                        return i;
-
-                    if (value > m)
-                        break;
-                }
-
-                return -1;
+        int low = 0,high = m;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int midN = func(mid,n,m);
+            if(midN==1){
+                return mid;
+            }
+            if(midN==2){
+                high = mid -1;
+            }
+            else low = mid +1;
             
+        }
+        return -1;
         
     }
 };
